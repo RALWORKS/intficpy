c039651db58995306d87c80283bef7d101e93f00
JSMaika <r.a.lester121@gmail.com>
2020-01-01 20:22:47 -0700

    tests - add basic tests for parser

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,15 +3,188 @@ import unittest
 from .helpers import IFPTestCase
 
 from intficpy.thing_base import Thing
-from intficpy.actor import Player
-from intficpy.room import Room
-from intficpy.vocab import english, verbDict, nounDict
-from intficpy.parser import getThing, initGame
+from intficpy.things import Surface
+from intficpy.vocab import nounDict
+from intficpy.parser import getThing, getCurVerb, getGrammarObj, callVerb
+from intficpy.verb import (
+    getVerb,
+    lookVerb,
+    setOnVerb,
+    leadDirVerb,
+    jumpOverVerb,
+    giveVerb,
+    examineVerb
+)
 
 
-class TestParser(IFPTestCase):
+class TestGetCurVerb(IFPTestCase):
+    def test_gets_correct_verb_with_no_objects(self):
+        tokens = lookVerb.syntax[0]
+
+        expected_verb_tokens = lookVerb.syntax[0]
+
+        gcv = getCurVerb(self.app, tokens)
+        found_verbs = gcv[1]
+        verb = gcv[0][0]
+        verb_tokens = gcv[0][1]
+
+        self.assertTrue(found_verbs)
+        self.assertEqual(verb_tokens, expected_verb_tokens)
+        self.assertIs(verb, lookVerb)
+
+    def test_gets_correct_verb_with_dobj_only(self):
+        tokens = self._insert_dobj_into_phrase(getVerb.syntax[0], ["yellow", "fish"])
+
+        expected_verb_tokens = getVerb.syntax[0]
+
+        gcv = getCurVerb(self.app, tokens)
+        found_verbs = gcv[1]
+        verb = gcv[0][0]
+        verb_tokens = gcv[0][1]
+
+        self.assertTrue(found_verbs)
+        self.assertEqual(verb_tokens, expected_verb_tokens)
+        self.assertIs(verb, getVerb)
+
+    def test_gets_correct_verb_with_dobj_and_direction_iobj(self):
+
+        dobj = Thing(self._get_unique_noun())
+        self.start_room.addThing(dobj)
+
+        tokens = self._insert_objects_into_phrase(
+            leadDirVerb.syntax[0],
+            [dobj.name],
+            ["north"]
+        )
+
+        expected_verb_tokens = leadDirVerb.syntax[0]
+
+        gcv = getCurVerb(self.app, tokens)
+        found_verbs = gcv[1]
+        verb = gcv[0][0]
+        verb_tokens = gcv[0][1]
+
+        self.assertTrue(found_verbs)
+        self.assertEqual(verb_tokens, expected_verb_tokens)
+        self.assertIs(verb, leadDirVerb)
+
+    def test_gets_correct_verb_with_prepopsition_dobj_only(self):
+        tokens = self._insert_dobj_into_phrase(
+            jumpOverVerb.syntax[0],
+            ["tall", "mountain"]
+        )
+
+        expected_verb_tokens = jumpOverVerb.syntax[0]
+
+        gcv = getCurVerb(self.app, tokens)
+        found_verbs = gcv[1]
+        verb = gcv[0][0]
+        verb_tokens = gcv[0][1]
+
+        self.assertTrue(found_verbs)
+        self.assertEqual(verb_tokens, expected_verb_tokens)
+        self.assertIs(verb, jumpOverVerb)
+
+    def test_gets_correct_verb_with_preposition_dobj_and_iobj(self):
+        tokens = self._insert_objects_into_phrase(
+            setOnVerb.syntax[0],
+            ["yellow", "fish"],
+            ["red", "shelf"]
+        )
+
+        expected_verb_tokens = setOnVerb.syntax[0]
+
+        gcv = getCurVerb(self.app, tokens)
+        found_verbs = gcv[1]
+        verb = gcv[0][0]
+        verb_tokens = gcv[0][1]
+
+        self.assertTrue(found_verbs)
+        self.assertEqual(verb_tokens, expected_verb_tokens)
+        self.assertIs(verb, setOnVerb)
+
+
+class TestGetGrammarObj(IFPTestCase):
+    def test_gets_correct_dobj(self):
+        expected_dobj = ["marvelous", "object"]
+
+        tokens = self._insert_dobj_into_phrase(getVerb.syntax[0], expected_dobj)
+        ggo = getGrammarObj(self.me, self.app, getVerb, tokens, getVerb.syntax[0])
+
+        self.assertTrue(ggo)
+        (dobj, iobj) = ggo
+
+        self.assertEqual(dobj, expected_dobj)
+
+    def test_gets_correct_objects_with_preposition(self):
+        expected_dobj = ["marvelous", "object"]
+        expected_iobj = ["wonderful", "item"]
+
+        tokens = self._insert_objects_into_phrase(
+            setOnVerb.syntax[0],
+            expected_dobj,
+            expected_iobj
+        )
+        ggo = getGrammarObj(self.me, self.app, setOnVerb, tokens, setOnVerb.syntax[0])
+
+        self.assertTrue(ggo)
+        (dobj, iobj) = ggo
+
+        self.assertEqual(dobj, expected_dobj)
+        self.assertEqual(iobj, expected_iobj)
+
+    def test_gets_correct_objects_with_adjacent_dobj_iobj(self):
+        dobj_item = Thing(self._get_unique_noun())
+        self.start_room.addThing(dobj_item)
+        iobj_item = Thing(self._get_unique_noun())
+        self.start_room.addThing(dobj_item)
+
+        expected_dobj = [dobj_item.name]
+        expected_iobj = [iobj_item.name]
+
+        tokens = self._insert_objects_into_phrase(
+            giveVerb.syntax[1],
+            expected_dobj,
+            expected_iobj
+        )
+        ggo = getGrammarObj(self.me, self.app, giveVerb, tokens, giveVerb.syntax[1])
+
+        self.assertTrue(ggo)
+        (dobj, iobj) = ggo
+
+        self.assertEqual(dobj, expected_dobj)
+        self.assertEqual(iobj, expected_iobj)
+
+    def test_gets_string_object(self):
+        dobj_item = Thing(self._get_unique_noun())
+        self.start_room.addThing(dobj_item)
+
+        expected_dobj = [dobj_item.name]
+        expected_iobj = ["west"]
+
+        tokens = self._insert_objects_into_phrase(
+            leadDirVerb.syntax[0],
+            expected_dobj,
+            expected_iobj
+        )
+        ggo = getGrammarObj(
+            self.me,
+            self.app,
+            leadDirVerb,
+            tokens,
+            leadDirVerb.syntax[0]
+        )
+
+        self.assertTrue(ggo)
+        (dobj, iobj) = ggo
+
+        self.assertEqual(dobj, expected_dobj)
+        self.assertEqual(iobj, expected_iobj)
+
+
+class TestCallVerb(IFPTestCase):
     def test_get_thing(self):
-        noun = "neverseenthisonebefore"
+        noun = self._get_unique_noun()
         self.assertNotIn(
             noun,
             nounDict,
@@ -55,6 +228,37 @@ class TestParser(IFPTestCase):
             "Noun adjective array should have been unambiguous, but failed to match "
             "Thing",
         )
+
+    def test_call_verb_with_no_objects(self):
+        obj_words = [None, None]
+
+        success = callVerb(self.me, self.app, lookVerb, obj_words)
+        self.assertTrue(success)
+
+    def test_call_verb_with_dobj(self):
+        dobj_item = Thing(self._get_unique_noun())
+        self.start_room.addThing(dobj_item)
+
+        dobj = [dobj_item.name]
+
+        obj_words = [dobj, None]
+
+        success = callVerb(self.me, self.app, examineVerb, obj_words)
+        self.assertTrue(success)
+
+    def test_call_verb_with_dobj_and_iobj(self):
+        dobj_item = Thing(self._get_unique_noun())
+        self.start_room.addThing(dobj_item)
+        iobj_item = Surface(self._get_unique_noun(), self.me)
+        self.start_room.addThing(iobj_item)
+
+        dobj = [dobj_item.name]
+        iobj = [iobj_item.name]
+
+        obj_words = [dobj, iobj]
+
+        success = callVerb(self.me, self.app, setOnVerb, obj_words)
+        self.assertTrue(success)
 
 
 if __name__ == "__main__":
