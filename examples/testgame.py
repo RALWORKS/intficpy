e2ca6b852b1137eb4227aa1262b5677d6266ea46
Rita Lester <r.a.lester121@gmail.com>
2021-01-06 09:47:35 -0700

    examples: update test game

--- examples/testgame.py
+++ examples/testgame.py
@@ -25,76 +25,85 @@ from intficpy.actor import Actor, Player, Topic, SpecialTopic
 from intficpy.ifp_game import IFPGame
 from intficpy.cli import TerminalApp
 
-me = Player("boy")
 ex = TerminalApp()
-game = IFPGame(me, ex)
+game = IFPGame(ex)
+me = Player(game)
+game.setPlayer(me)
 
-
-game.aboutGame.title = "WIND AND OCEAN"
+# Set the basic game information
+game.aboutGame.title = "Demo Game"
 game.aboutGame.author = "JSMaika"
 
-seenshackintro = False
+# OPENING
+def opening(game):
+    # Text is fed to to App to be shown to the player in bundles called "events".
+    # All events are printed at the end of the turn, ordered by their priority, from
+    # lowest number, to highest.
+    # Every turn will have 2 events by default - the "turn" event, where IFP puts the
+    # player's actions and their outcomes, and the "command" event, where the player
+    # command is echoed
+    # To add text to the next turn, can, 1) add text to an existing event, with
+    #   game.addTextToEvent("some event name", "New text to append!")
+    # or, its shortcut for adding to the "turn" event
+    #   game.addText("We'll tack this onto the turn.")
+    # or, (2) you can add a new, named event, as shown below.
+    game.addEvent(
+        "opening",  # the name of your event
+        1,  # the priority of the event. lower numbers will show first. the main turn event is 5
+        (
+            "You can hear the waves crashing on the shore outside. "
+            "There are no car sounds, no human voices."
+            "You are far from any populated area."
+        ),
+    )
 
-opalAchievement = Achievement(2, "finding the opal")
-keyAchievement = Achievement(2, "receiving the key")
 
+game.gameOpening = opening  # this function will be called once when the game begins
+
+
+# ACHIEVEMENTS
+opalAchievement = Achievement(game, 2, "finding the opal")
+keyAchievement = Achievement(game, 2, "receiving the key")
+
+# ENDINGS
 freeEnding = Ending(
-    True,
+    game,
+    True,  # this is a good ending
     "**YOU ARE FREE**",
     "You arrive on the seashore, leaving Sarah to cower in her shack. You are free.",
 )
 
-startroom = Room(
-    "Shack interior",
-    "You are standing in a one room shack. Light filters in through a cracked, dirty window. ",
-)
-shack_concept = Abstract("shack")
-shack_key_concept = Abstract("key")
-shack_key_concept.setAdjectives(["shack", "door"])
-shore_concept = Abstract("shore")
-shore_concept.addSynonym("outside")
+# PLOT ESSENTIAL PROPS
+silverkey = Key(game)
+silverkey.setAdjectives(["silver"])
 
-startroom.addThing(me)
-me.setPlayer()
+rustykey = Key(game)
+rustykey.setAdjectives(["rusty"])
 
-
-def opening(game):
-    game.addTextToEvent(
-        "turn",
-        "<b>WIND AND OCEAN: by JSMaika</b><br> You can hear the waves crashing on the shore outside. There are no car sounds, no human voices. You are far from any populated area.",
-    )
-
-
-game.gameOpening = opening
-
-
-def addCave(game):
-    cave_concept.makeKnown(me)
-
-
-scarf = Clothing("scarf")
-startroom.addThing(scarf)
-
-box = Container("box")
-box.can_contain_standing_player = True
-box.can_contain_sitting_player = True
-box.can_contain_lying_player = True
-box.giveLid()
-startroom.addThing(box)
-
-opal = Thing("opal")
+opal = Thing(game, "opal")
 opal.makeUnique()
 opal.size = 25
 
-box.addThing(opal)
+# ROOMS
 
-me.opaltaken = False
+# Start Room (Shack Interior)
+startroom = Room(
+    game,
+    "Shack interior",
+    "You are standing in a one room shack. Light filters in through a cracked, dirty window. ",
+)
+
+me.moveTo(startroom)
+
+storm_concept = Abstract(game, "storm")
+shack_concept = Abstract(game, "shack")
+shack_concept.setAdjectives(["one", "room"])
+shack_concept.makeKnown(me)
 
 
 def takeOpalFunc(game):
     if not me.opaltaken:
-        game.addTextToEvent(
-            "turn",
+        game.addText(
             "As you hold the opal in your hand, you're half-sure you can feel the air cooling around you. A shiver runs down your spine. Something is not right here.",
         )
         me.opaltaken = True
@@ -103,57 +112,62 @@ def takeOpalFunc(game):
 
 opal.getVerbDobj = takeOpalFunc
 
-bottle = Thing("bottle")
-bottle.setAdjectives(["old"])
-startroom.addThing(bottle)
-
-bottle2 = bottle.copyThing()
-startroom.addThing(bottle2)
-
-bench = Surface("bench")
+bench = Surface(game, "bench")
 bench.can_contain_sitting_player = True
 bench.can_contain_standing_player = True
 bench.invItem = False
-bench.describeThing("A rough wooden bench sits against the wall.")
+bench.describeThing("A rough wooden bench sits against the wall. ")
 bench.xdescribeThing(
     "The wooden bench is splintering, and faded grey. It looks very old."
 )
-startroom.addThing(bench)
-underbench = UnderSpace("space")
+bench.moveTo(startroom)
+
+underbench = UnderSpace(game, "space")
 underbench.contains_preposition = "in"
 bench.addComposite(underbench)
+# UnderSpaces that are components of another item are not described if their "description"
+# attribute is None
+# This also means that we cannot see what's underneath
+# Set description to an empty string so we can show what's underneath without
+# having to print an actual description of the UnderSpace
+underbench.description = ""
 underbench._verbose_name = "space under the bench"
 
-nails = Thing("nails")
-nails.addSynonym("can")
-nails.setAdjectives(["can", "of"])
-bench.addThing(nails)
+box = Container(game, "box")
+box.giveLid()
+box.moveTo(underbench)
 
-emptycan = Container("can")
-emptycan.setAdjectives(["empty", "old"])
-emptycan.size = 30
-startroom.addThing(emptycan)
+opal.moveTo(box)
 
+boxlock = Lock(game, True, silverkey)
+box.setLock(boxlock)
+
+# Beach
 beach = OutdoorRoom(
-    "Beach, near the shack", "You find yourself on an abandoned beach. "
+    game, "Beach, near the shack", "You find yourself on an abandoned beach. "
 )
 
 
-def beachEnding(game):
+def beachArrival(game):
     freeEnding.endGame(game)
 
 
-beach.arriveFunc = beachEnding
+beach.arriveFunc = beachArrival
 
-shackdoor = DoorConnector(startroom, "e", beach, "w")
+shackdoor = DoorConnector(game, startroom, "e", beach, "w")
 shackdoor.entranceA.describeThing("To the east, a door leads outside. ")
 shackdoor.entranceB.describeThing("The door to the shack is directly west of you. ")
+
+cabinlock = Lock(game, True, rustykey)
+shackdoor.setLock(cabinlock)
 
 startroom.exit = shackdoor
 beach.entrance = shackdoor
 
-attic = Room("Shack, attic", "You are in a dim, cramped attic.")
-shackladder = LadderConnector(startroom, attic)
+# Attic
+
+attic = Room(game, "Shack, attic", "You are in a dim, cramped attic. ")
+shackladder = LadderConnector(game, startroom, attic)
 shackladder.entranceA.describeThing(
     "Against the north wall is a ladder leading up to the attic. "
 )
@@ -161,96 +175,126 @@ shackladder.entranceA.xdescribeThing(
     "Against the north wall is a ladder leading up to the attic. "
 )
 startroom.north = shackladder
+silverkey.moveTo(attic)
 
-rustykey = Key()
-rustykey.setAdjectives(["rusty"])
-# attic.addThing(rustykey)
-cabinlock = Lock(True, rustykey)
+# CHARACTERS
+# Sarah
+sarah = Actor(game, "Sarah")
+sarah.makeProper("Sarah")
+sarah.moveTo(startroom)
 
 
 def sarahOpalFunc(game, dobj):
+    """
+    IntFicPy verbs can be overridden or customized for specific items.
+    To do this, find the verb you want to customize in the intficpy.verb module.
+    Note
+        1) the class name of the verb
+        2) whether its verbFunc method takes just a "dobj" (direct object) parameter,
+           or an "iobj" (indirect object) as well
+           For instance, GetVerb (as in, "get opal") takes only a direct object (here, "opal"),
+           while GiveVerb (as in, "give opal to Sarah") takes an indirect object as well
+           (here, "Sarah")
+    Decide whether you want to override the verb for the direct, or indirect object.
+    Create a new function in your game file. The first parameter will always be "game".
+    If you are overriding for an indirect object, you will also need to accept a "dobj"
+    parameter for the indirect object. Likewise, if you are overriding for the direct
+    object of a verb that has an indirect object as well, you should accept an "iobj"
+    parameter to receive the indirect object.
+
+    Inside the function, you can perform whatever tasks you want to do to create your
+    custom behaviour.
+
+    Return True to skip the verb's normal behaviour after evaluating, or False to continue
+    as normal.
+    """
     if not sarah.threwkey and dobj == sarah:
-        game.addTextToEvent(
-            "turn",
-            '"Fine!" she cries. "Fine! Take the key and leave! Just get that thing away from me!" ',
+        game.addText(
+            '"Fine!" she cries. "Fine! Take the key and leave! "'
+            '"Just get that thing away from me!" ',
         )
-        game.addTextToEvent("turn", "Sarah flings a rusty key at you. You catch it.")
-        me.addThing(rustykey)
+        game.addText("Sarah flings a rusty key at you. You catch it.")
+        rustykey.moveTo(me)
         keyAchievement.award(game)
         sarah.threwkey = True
+        return True
 
 
+# Now that we've created our custom behaviour, we need to hook it up to evaluate when
+# the verb is called on our direct or indirect object
+# When creating the override function, we should have noted to class name of the IFP
+# verb we're overriding.
+# In this case, we're creating an override for GiveVerb and ShowVerb for the
+# indirect object of the opal
+# To do this, we will need to add an attribute to the opal for each verb we are overriding.
+# The verb looks for overrides by chacking its objects for attributes with the following
+# pattern:
+#   [firstLetterLoweredClassNameOfVerb]Iobj
+# or for direct object overrides:
+#   [firstLetterLoweredClassNameOfVerb]Dobj
+# so, for the indirect object for GiveVerb, we get the attribute, "giveVerbDobj"
+# All we need to do now is assign our new function to the attribute name we derived
 opal.giveVerbIobj = sarahOpalFunc
 opal.showVerbIobj = sarahOpalFunc
 
-shackdoor.setLock(cabinlock)
-silverkey = Key()
-silverkey.setAdjectives(["silver"])
-boxlock = Lock(True, silverkey)
-box.setLock(boxlock)
-attic.addThing(silverkey)
-
-rock = Thing("rock")
-beach.addThing(rock)
-
-sarah = Actor("Sarah")
-sarah.makeProper("Sarah")
-startroom.addThing(sarah)
 howgethere = SpecialTopic(
+    game,
     "ask how you got here",
     'You ask Sarah how you got here. She bites her lip. <br> "There was a storm," she says. "Your ship crashed on shore. I brought you inside."',
 )
-sarah.threwkey = False
 
 
 def sarahDefault(game):
-    game.addTextToEvent("turn", sarah.default_topic)
+    game.addText(sarah.default_topic)
+    storm_concept.makeKnown(me)
     sarah.addSpecialTopic(howgethere)
 
 
 sarah.defaultTopic = sarahDefault
 
 opalTopic = Topic(
-    '"I should never it from the cave," says Sarah. "I want nothing to do with it. If you\'re smart, you\'ll leave it where you found it."'
+    game,
+    '"I should never it from the cave," says Sarah. "I want nothing to do with it. If you\'re smart, you\'ll leave it where you found it."',
 )
 sarah.addTopic("asktell", opalTopic, opal)
 
 shackTopic = Topic(
-    '"It\'s not such a bad place, this shack," Sarah says. "It\'s warm. Safe."'
+    game, '"It\'s not such a bad place, this shack," Sarah says. "It\'s warm. Safe."'
 )
 sarah.addTopic("asktell", shackTopic, shack_concept)
 
-outsideTopic = Topic(
-    '"You wouldn\'t want to go out there," Sarah says. "You\'re inside for a reason. The rest of the world isn\'t safe for you."'
-)
-sarah.addTopic("asktell", outsideTopic, shore_concept)
-
-keyTopic = Topic('"Key?" Sarah says. "What key? I haven\'t got any key."')
-sarah.addTopic("asktell", keyTopic, shack_key_concept)
-sarah.addTopic("asktell", keyTopic, cabinlock)
-sarah.addTopic("asktell", keyTopic, shackdoor.entranceA)
-
 key2Topic = Topic(
-    '"Leave that be," Sarah says, a hint of anxiety in her voice. "Some things are better left untouched."'
+    game,
+    '"Leave that be," Sarah says, a hint of anxiety in her voice. "Some things are better left untouched."',
 )
 sarah.addTopic("asktellgiveshow", key2Topic, silverkey)
 
 sarahTopic = Topic(
-    '"You want to know about <i>me?</i>" Sarah says. "I\'m flattered. Just think of me as your protector."'
+    game,
+    '"You want to know about <i>me?</i>" Sarah says. "I\'m flattered. Just think of me as your protector."',
 )
 sarah.addTopic("asktell", sarahTopic, sarah)
 
 meTopic = Topic(
-    '"You were in a sorry state when I found you," Sarah says. "Be glad I brought you here."'
+    game,
+    '"You were in a sorry state when I found you," Sarah says. "Be glad I brought you here."',
 )
 sarah.addTopic("asktell", meTopic, me)
 
 sarah.default_topic = "Sarah scoffs."
 
-cave_concept = Abstract("cave")
+stormTopic = Topic(
+    game, 'Sarah narrows her eyes. "Yes, there was a storm yesterday." she says.'
+)
+sarah.addTopic("ask", stormTopic, storm_concept)
 
-caveTopic = Topic('Sarah narrows her eyes. "You don\'t want to know," she says.')
-sarah.addTopic("ask", caveTopic, cave_concept)
+# PROGRESS TRACKING
+# IntFicPy is not able to save global variables, or objects that do not inherit from an IFP
+# base class
+# However, IFP can save attributes added to IFP objects, such as items or characters
+# This is one of the easiest ways to save progress or player choice
+me.opaltaken = False
+sarah.threwkey = False
 
-
+# now that all our objects are set up, run the game
 ex.runGame()
