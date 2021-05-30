17a3ae01f9330a0591f44845f92fc8ded06497a0
Rita Lester <r.a.lester121@gmail.com>
2021-05-29 21:39:38 -0600

    test: clean up conversation tests: split test cases

--- tests/verbs/test_conversation.py
+++ tests/verbs/test_conversation.py
@@ -66,185 +66,6 @@ class TestGetImpTalkToUnambiguous(IFPTestCase):
         self.assertIs(self.game.parser.previous_command.dobj.target, self.actor1)
 
 
-class TestConversationVerbs(IFPTestCase):
-    def setUp(self):
-        super().setUp()
-        self.item = Thing(self.game, "mess")
-        self.actor = Actor(self.game, "Jenny")
-        self.start_room.addThing(self.item)
-        self.start_room.addThing(self.actor)
-        self.CANNOT_TALK_MSG = "You cannot talk to that. "
-        self.topic = Topic(self.game, '"Ah, yes," says Jenny mysteriously. ')
-
-    def test_ask_inanimate(self):
-        AskVerb()._runVerbFuncAndEvents(self.game, self.item, self.actor)
-        msg = self.app.print_stack.pop()
-        self.assertEqual(
-            msg,
-            self.CANNOT_TALK_MSG,
-            "Tried ask verb with an inanimate dobj. Expected msg "
-            f"{self.CANNOT_TALK_MSG}, received {msg}",
-        )
-
-    def test_tell_inanimate(self):
-        TellVerb()._runVerbFuncAndEvents(self.game, self.item, self.actor)
-        msg = self.app.print_stack.pop()
-        self.assertEqual(
-            msg,
-            self.CANNOT_TALK_MSG,
-            "Tried ask verb with an inanimate dobj. Expected msg "
-            f"{self.CANNOT_TALK_MSG}, received {msg}",
-        )
-
-    def test_give_inanimate(self):
-        GiveVerb()._runVerbFuncAndEvents(self.game, self.item, self.item)
-        msg = self.app.print_stack.pop()
-        self.assertEqual(
-            msg,
-            self.CANNOT_TALK_MSG,
-            "Tried ask verb with an inanimate dobj. Expected msg "
-            f"{self.CANNOT_TALK_MSG}, received {msg}",
-        )
-
-    def test_show_inanimate(self):
-        ShowVerb()._runVerbFuncAndEvents(self.game, self.item, self.item)
-        msg = self.app.print_stack.pop()
-        self.assertEqual(
-            msg,
-            self.CANNOT_TALK_MSG,
-            "Tried ask verb with an inanimate dobj. Expected msg "
-            f"{self.CANNOT_TALK_MSG}, received {msg}",
-        )
-
-    def test_ask_no_defined_topic(self):
-        self.actor.defaultTopic(self.game)
-        self.game.runTurnEvents()
-        default = self.app.print_stack.pop()
-
-        self.assertNotIn(
-            self.item.ix, self.actor.ask_topics,
-        )
-
-        AskVerb()._runVerbFuncAndEvents(self.game, self.actor, self.item)
-        msg = self.app.print_stack.pop()
-
-        self.assertEqual(
-            msg,
-            default,
-            "Tried ask verb for topic not in ask topics. Expected default topic "
-            f"{default}, received {msg}",
-        )
-
-    def test_tell_no_defined_topic(self):
-        self.actor.defaultTopic(self.game)
-        self.game.runTurnEvents()
-        default = self.app.print_stack.pop()
-
-        self.assertNotIn(
-            self.item.ix, self.actor.tell_topics,
-        )
-
-        TellVerb()._runVerbFuncAndEvents(self.game, self.actor, self.item)
-        msg = self.app.print_stack.pop()
-
-        self.assertEqual(
-            msg,
-            default,
-            "Tried tell verb for topic not in tell topics. Expected default topic "
-            f"{default}, received {msg}",
-        )
-
-    def test_give_no_defined_topic(self):
-        self.actor.defaultTopic(self.game)
-        self.game.runTurnEvents()
-        default = self.app.print_stack.pop()
-
-        self.assertNotIn(
-            self.item.ix, self.actor.give_topics,
-        )
-
-        GiveVerb()._runVerbFuncAndEvents(self.game, self.actor, self.item)
-        msg = self.app.print_stack.pop()
-
-        self.assertEqual(
-            msg,
-            default,
-            "Tried give verb for topic not in give topics. Expected default topic "
-            f"{default}, received {msg}",
-        )
-
-    def test_show_no_defined_topic(self):
-        self.actor.defaultTopic(self.game)
-        self.game.runTurnEvents()
-        default = self.app.print_stack.pop()
-
-        self.assertNotIn(
-            self.item.ix, self.actor.show_topics,
-        )
-
-        ShowVerb()._runVerbFuncAndEvents(self.game, self.actor, self.item)
-        msg = self.app.print_stack.pop()
-
-        self.assertEqual(
-            msg,
-            default,
-            "Tried show verb for topic not in show topics. Expected default topic "
-            f"{default}, received {msg}",
-        )
-
-    def test_ask_with_topic(self):
-        self.actor.addTopic("ask", self.topic, self.item)
-
-        AskVerb()._runVerbFuncAndEvents(self.game, self.actor, self.item)
-        msg = self.app.print_stack.pop()
-
-        self.assertEqual(
-            msg,
-            self.topic.text,
-            "Tried ask verb for topic in ask topics. Expected topic text "
-            f"{self.topic.text}, received {msg}",
-        )
-
-    def test_tell_with_topic(self):
-        self.actor.addTopic("tell", self.topic, self.item)
-
-        TellVerb()._runVerbFuncAndEvents(self.game, self.actor, self.item)
-        msg = self.app.print_stack.pop()
-
-        self.assertEqual(
-            msg,
-            self.topic.text,
-            "Tried tell verb for topic in ask topics. Expected topic text "
-            f"{self.topic.text}, received {msg}",
-        )
-
-    def test_give_with_topic(self):
-        self.actor.addTopic("give", self.topic, self.item)
-
-        GiveVerb()._runVerbFuncAndEvents(self.game, self.actor, self.item)
-        msg = self.app.print_stack.pop()
-
-        self.assertEqual(
-            msg,
-            self.topic.text,
-            "Tried give verb for topic in ask topics. Expected topic text "
-            f"{self.topic.text}, received {msg}",
-        )
-
-    def test_show_with_topic(self):
-        self.actor.addTopic("show", self.topic, self.item)
-
-        ShowVerb()._runVerbFuncAndEvents(self.game, self.actor, self.item)
-        msg = self.app.print_stack.pop()
-
-        self.assertEqual(
-            msg,
-            self.topic.text,
-            "Tried show verb for topic in ask topics. Expected topic text "
-            f"{self.topic.text}, received {msg}",
-        )
-
-
 class TestSpecialTopic(IFPTestCase):
     def setUp(self):
         super().setUp()
@@ -305,3 +126,207 @@ class TestTalkTo(IFPTestCase):
         self.actor.return_hi_topic = Topic(self.game, "You're back. Great.")
         self.game.turnMain("hi girl")
         self.assertIn(self.actor.return_hi_topic.text, self.app.print_stack)
+
+
+class TestAskAbout(IFPTestCase):
+    def setUp(self):
+        super().setUp()
+        self.actor = Actor(self.game, "girl")
+        self.actor.moveTo(self.start_room)
+        self.item = Thing(self.game, "mess")
+        self.item.moveTo(self.start_room)
+        self.CANNOT_TALK_MSG = "You cannot talk to that. "
+        self.topic = Topic(self.game, '"Ah, yes," says the girl mysteriously. ')
+        self.game.turnMain("l")
+
+    def test_ask_with_topic(self):
+        self.actor.addTopic("ask", self.topic, self.item)
+
+        self.game.turnMain("ask girl about mess")
+        msg = self.app.print_stack.pop()
+
+        self.assertEqual(
+            msg,
+            self.topic.text,
+            "Tried ask verb for topic in ask topics. Expected topic text "
+            f"{self.topic.text}, received {msg}",
+        )
+
+    def test_ask_no_defined_topic(self):
+        self.assertNotIn(
+            self.item.ix, self.actor.ask_topics,
+        )
+
+        self.game.turnMain("ask girl about mess")
+        msg = self.app.print_stack.pop()
+
+        self.assertEqual(
+            msg,
+            self.actor.default_topic,
+            "Tried ask verb for topic not in ask topics. Expected default topic "
+            f"{self.actor.default_topic}, received {msg}",
+        )
+
+    def test_ask_inanimate(self):
+        self.game.turnMain("ask mess about girl")
+        msg = self.app.print_stack.pop()
+        self.assertEqual(
+            msg,
+            self.CANNOT_TALK_MSG,
+            "Tried ask verb with an inanimate dobj. Expected msg "
+            f"{self.CANNOT_TALK_MSG}, received {msg}",
+        )
+
+
+class TestTellAbout(IFPTestCase):
+    def setUp(self):
+        super().setUp()
+        self.actor = Actor(self.game, "girl")
+        self.actor.moveTo(self.start_room)
+        self.item = Thing(self.game, "mess")
+        self.item.moveTo(self.start_room)
+        self.CANNOT_TALK_MSG = "You cannot talk to that. "
+        self.topic = Topic(self.game, '"Ah, yes," says the girl mysteriously. ')
+        self.game.turnMain("l")
+
+    def test_tell_with_topic(self):
+        self.actor.addTopic("tell", self.topic, self.item)
+
+        self.game.turnMain("tell girl about mess")
+        msg = self.app.print_stack.pop()
+
+        self.assertEqual(
+            msg,
+            self.topic.text,
+            "Tried tell verb for topic in ask topics. Expected topic text "
+            f"{self.topic.text}, received {msg}",
+        )
+
+    def test_tell_no_defined_topic(self):
+        self.assertNotIn(
+            self.item.ix, self.actor.tell_topics,
+        )
+
+        self.game.turnMain("tell girl about mess")
+        msg = self.app.print_stack.pop()
+
+        self.assertEqual(
+            msg,
+            self.actor.default_topic,
+            "Tried tell verb for topic not in tell topics. Expected default topic "
+            f"{self.actor.default_topic}, received {msg}",
+        )
+
+    def test_tell_inanimate(self):
+        self.game.turnMain("tell mess about girl")
+        msg = self.app.print_stack.pop()
+        self.assertEqual(
+            msg,
+            self.CANNOT_TALK_MSG,
+            "Tried ask verb with an inanimate dobj. Expected msg "
+            f"{self.CANNOT_TALK_MSG}, received {msg}",
+        )
+
+
+class TestGive(IFPTestCase):
+    def setUp(self):
+        super().setUp()
+        self.actor = Actor(self.game, "girl")
+        self.actor.moveTo(self.start_room)
+        self.item = Thing(self.game, "mess")
+        self.item.moveTo(self.start_room)
+        self.CANNOT_TALK_MSG = "You cannot talk to that. "
+        self.topic = Topic(self.game, '"Ah, yes," says the girl mysteriously. ')
+        self.game.turnMain("l")
+
+    def test_give_no_defined_topic(self):
+        default = self.actor.default_topic
+
+        self.assertNotIn(
+            self.item.ix, self.actor.give_topics,
+        )
+
+        GiveVerb()._runVerbFuncAndEvents(self.game, self.actor, self.item)
+        msg = self.app.print_stack.pop()
+
+        self.assertEqual(
+            msg,
+            default,
+            "Tried give verb for topic not in give topics. Expected default topic "
+            f"{default}, received {msg}",
+        )
+
+    def test_give_with_topic(self):
+        self.actor.addTopic("give", self.topic, self.item)
+
+        GiveVerb()._runVerbFuncAndEvents(self.game, self.actor, self.item)
+        msg = self.app.print_stack.pop()
+
+        self.assertEqual(
+            msg,
+            self.topic.text,
+            "Tried give verb for topic in ask topics. Expected topic text "
+            f"{self.topic.text}, received {msg}",
+        )
+
+    def test_give_inanimate(self):
+        GiveVerb()._runVerbFuncAndEvents(self.game, self.item, self.item)
+        msg = self.app.print_stack.pop()
+        self.assertEqual(
+            msg,
+            self.CANNOT_TALK_MSG,
+            "Tried ask verb with an inanimate dobj. Expected msg "
+            f"{self.CANNOT_TALK_MSG}, received {msg}",
+        )
+
+
+class TestShow(IFPTestCase):
+    def setUp(self):
+        super().setUp()
+        self.actor = Actor(self.game, "girl")
+        self.actor.moveTo(self.start_room)
+        self.item = Thing(self.game, "mess")
+        self.item.moveTo(self.start_room)
+        self.CANNOT_TALK_MSG = "You cannot talk to that. "
+        self.topic = Topic(self.game, '"Ah, yes," says the girl mysteriously. ')
+        self.game.turnMain("l")
+
+    def test_show_with_topic(self):
+        self.actor.addTopic("show", self.topic, self.item)
+
+        ShowVerb()._runVerbFuncAndEvents(self.game, self.actor, self.item)
+        msg = self.app.print_stack.pop()
+
+        self.assertEqual(
+            msg,
+            self.topic.text,
+            "Tried show verb for topic in ask topics. Expected topic text "
+            f"{self.topic.text}, received {msg}",
+        )
+
+    def test_show_no_defined_topic(self):
+        default = self.actor.default_topic
+
+        self.assertNotIn(
+            self.item.ix, self.actor.show_topics,
+        )
+
+        ShowVerb()._runVerbFuncAndEvents(self.game, self.actor, self.item)
+        msg = self.app.print_stack.pop()
+
+        self.assertEqual(
+            msg,
+            default,
+            "Tried show verb for topic not in show topics. Expected default topic "
+            f"{default}, received {msg}",
+        )
+
+    def test_show_inanimate(self):
+        ShowVerb()._runVerbFuncAndEvents(self.game, self.item, self.item)
+        msg = self.app.print_stack.pop()
+        self.assertEqual(
+            msg,
+            self.CANNOT_TALK_MSG,
+            "Tried ask verb with an inanimate dobj. Expected msg "
+            f"{self.CANNOT_TALK_MSG}, received {msg}",
+        )
