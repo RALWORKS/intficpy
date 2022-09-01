d9a34a37da93217d834182ee1971b2e364dc354e
Rita Lester <r.a.lester121@gmail.com>
2022-09-01 08:50:03 -0600

    test: endings

--- tests/test_score.py
+++ tests/test_score.py
@@ -1,6 +1,6 @@
 from .helpers import IFPTestCase
 
-from intficpy.score import Achievement
+from intficpy.score import Achievement, Ending
 from intficpy.things import Thing
 
 
@@ -18,3 +18,34 @@ class TestAchevement(IFPTestCase):
         self.assertEqual(self.game.score.total, 3)
         self.game.turnMain("score")
         self.game.turnMain("fullscore")
+
+
+class TestEnding(IFPTestCase):
+    def test_end_game(self):
+        end = Ending(self.game, True, "Won!", "You won!")
+
+        item = Thing(self.game, "disc")
+        item.moveTo(self.start_room)
+        item.getVerbDobj = lambda x: end.endGame(self.game)
+
+        self.assertFalse(self.game.ended)
+
+        self.game.turnMain("take disc")
+
+        self.assertTrue(self.game.ended)
+
+        self.game.turnMain("drop disc")
+
+        self.assertIn("The game has ended", self.app.print_stack.pop())
+
+        self.game.turnMain("score")
+        self.assertNotIn("The game has ended", self.app.print_stack.pop())
+
+        self.game.turnMain("fullscore")
+        self.assertNotIn("The game has ended", self.app.print_stack.pop())
+
+        self.game.turnMain("full score")
+        self.assertNotIn("The game has ended", self.app.print_stack.pop())
+
+        self.game.turnMain("about")
+        self.assertNotIn("The game has ended", self.app.print_stack.pop())
